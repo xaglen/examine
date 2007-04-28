a2f57f2fa8ac2b038498759090ce5f65f4fbe40d
glen.davis <glen.davis@46962259-042f-0410-9c2a-95ac4a8bd949>
2007-04-28 00:06:21 +0000

    rolled siteTools function into functions.php, converted line endings in all files to unix convention

--- trunk/includes/functions.php
+++ trunk/includes/functions.php
@@ -172,4 +172,19 @@ function getevent_attendance($event_id=NULL) {
 	$count=$db->getOne($sql);
 	return $count;
 }
+
+//
+//  obscureEmail()
+//  Modifies an email address to make it less spam-scraper-friendly
+//  usage: despam($email[, $linkText])
+//  returns a complete mailto link
+//
+function obscureEmail($email) {
+	$partA = substr($email, 0, strpos($email, '@'));
+	$partB = substr($email, strpos($email, '@'));
+	$partB = rtrim($partB);
+	$linkText = (func_num_args() == 2) ? func_get_arg(1) : $email;
+	$linkText = str_replace('@', '<span class="obscure">&#64;</span> ', $linkText);
+	return '<a href="email" onClick=\'a="'.$partA.'";this.href="ma"+"il"+"to:"+a+"'.$partB.'";\'>'.$linkText.'</a>';
+}
 ?>
