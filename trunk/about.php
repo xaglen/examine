20033c2bf49f81efa935f7d485e5bf033e6dcb41
glen.davis <glen.davis@46962259-042f-0410-9c2a-95ac4a8bd949>
2007-04-29 14:52:12 +0000

    added some files for appearance

---
+++ trunk/about.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>About eXAmine</title>
+<link rel="stylesheet" href="examine.css" type="text/css">
+</head>
+<body>
+<?php include 'templates/header.php'?>
+<div id="main">
+<p><b>eXAmine</b> is a web-based administration tool for your Chi Alpha ministry.</p>
+<h3>Privacy</h3>
+<p>We take your privacy and the privacy of your students very seriously. To that end, we make the following commitment:
+the national office cannot see your data. Neither can your area director nor can your DXAR. The only people other than you who can view your ministry's data are your fellow staff members (if any).</p>
+</div>
+<?php include 'templates/footer.php'?>
+</body>
+</html>
