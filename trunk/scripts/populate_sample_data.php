1687b975c439306b87452ffbdcb58814fe6ab4e3
glen.davis <glen.davis@46962259-042f-0410-9c2a-95ac4a8bd949>
2007-04-30 22:36:54 +0000

    created some basic sample data and caught some bugs

--- trunk/scripts/populate_sample_data.php
+++ trunk/scripts/populate_sample_data.php
@@ -1,10 +1,56 @@
 <?php
-require_once basename(__FILE__).'/../config.php';
+require_once dirname(__FILE__).'/../config.php';
+require_once dirname(__FILE__).'/../includes/functions.php';
 require_once 'Auth.php';
+require_once 'MDB2.php';
 
+//$db=createDB();
+
+//$types=array('integer','text','text','integer','text');
+//$db->prepare('REPLACE INTO TABLE people (people_id,first_name,last_name,category_id,gender) VALUES (?,?,?,?,?)',$types, MDB2_PREPARE_MANIP);
+?>
+<HTML>
+<HEAD>
+<TITLE>Refresh Sample Data</TITLE>
+</HEAD>
+<BODY>
+<?php
+
+$db = createDB();
+
+$sql = "LOAD DATA LOW_PRIORITY LOCAL INFILE 'data/sample.ministries.csv' REPLACE INTO TABLE ministries FIELDS TERMINATED BY ';' OPTIONALLY ENCLOSED BY '\"'";
+echo $sql.'<br/>';
+$db->query($sql);
+
+$sql = "LOAD DATA LOW_PRIORITY LOCAL INFILE 'data/sample.people.csv' REPLACE INTO TABLE people FIELDS TERMINATED BY ';' OPTIONALLY ENCLOSED BY '\"'"; 
+echo $sql.'<br/>';
+$db->query($sql);
+
+$sql = "LOAD DATA LOW_PRIORITY LOCAL INFILE 'data/sample.schools.csv' REPLACE INTO TABLE schools FIELDS TERMINATED BY ';' OPTIONALLY ENCLOSED BY '\"'";
+echo $sql.'<br/>';
+$db->query($sql);
+
+$sql =  "LOAD DATA LOW_PRIORITY LOCAL INFILE 'data/sample.ministry.people.csv' REPLACE INTO TABLE ministry_people FIELDS TERMINATED BY ';' OPTIONALLY ENCLOSED BY '\"'";
+echo $sql.'<br/>';
+$db->query($sql);
+
+$sql =  "LOAD DATA LOW_PRIORITY LOCAL INFILE 'data/sample.events.csv' REPLACE INTO TABLE events FIELDS TERMINATED BY ';' OPTIONALLY ENCLOSED BY '\"'";
+echo $sql.'<br/>';
+$db->query($sql);
+
+$sql =  "LOAD DATA LOW_PRIORITY LOCAL INFILE 'data/sample.event.attendance.csv' REPLACE INTO TABLE event_attendance FIELDS TERMINATED BY ';' OPTIONALLY ENCLOSED BY '\"'";
+echo $sql.'<br/>';
+$db->query($sql);
+
+$sql = "LOAD DATA LOW_PRIORITY LOCAL INFILE 'data/sample.users.csv' REPLACE INTO TABLE users FIELDS TERMINATED BY ';' OPTIONALLY ENCLOSED BY '\"'";
+echo $sql.'<br/>';
+$db->query($sql);
+
+// set up example login user - now defunct. Keep for reference.
+/*
 $a = &new Auth("MDB2", $loginOptions);
-$exampleUser = 'example@chialpha.com';
-$examplePass = 'demo';
+$exampleUser = 'example@chialpha.com";
+$examplePass = 'demo";
 $users = $a->listUsers();
 
 // if the user already exists then reset the password, otherwise create
@@ -20,4 +66,7 @@ if ($userExists) {
 } else {
     $a->addUser($exampleUser, $examplePass);
 }
+*/
 ?>
+</BODY>
+</HTML>
