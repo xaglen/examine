2ea063fcad6bd00a3b68f21e9098cb50de80c4c3
glen.davis <glen.davis@46962259-042f-0410-9c2a-95ac4a8bd949>
2007-07-10 22:47:45 +0000

    Trying to snazz up buttons

--- templates/header.php
+++ templates/header.php
@@ -7,13 +7,16 @@ $thisfile=basename($_SERVER['PHP_SELF']);
 <?php 
 	if ($thisfile=='events.php') {
 		?>
+		<!-- this turns out to not be very helpful...
 		<li><span>events</span>
 		<ul id="secondary">
 		<li><a href="#" onclick="javascript:editmode()">edit</a></li>
 		<li><a href='<?php echo $_SERVER['PHP_SELF'];?>?action=delete&amp;event_id='<?php echo $event_id;?>' onclick="javascript:return confirm('Are you sure you want to delete this module?')">delete</a></li>
 		<li><a href='<?php echo $_SERVER['PHP_SELF'];?>'?action=add>add a new event</a></li>
 		</ul>
+		-->
 		<?php
+		echo '<li><a href="events.php">events</a></li>';
 	} else {
 		echo '<li><a href="events.php">events</a></li>';
 	}
