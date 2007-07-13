c811680d4209fc3e1d52917d1bf7b97590d2afac
glen.davis <glen.davis@46962259-042f-0410-9c2a-95ac4a8bd949>
2007-07-13 04:30:07 +0000

    shifted code around for smoother logic flow

--- trunk/events.php
+++ trunk/events.php
@@ -14,8 +14,11 @@ require_once 'HTML/QuickForm/Renderer/Tableless.php';
 require_once 'includes/functions.php';
 require_once 'includes/functions.time.php';
 
-$db=createDB();
+$title='Events';
 $message='';
+$headerDetails='';
+
+$db=createDB();
 
 $log->log($_REQUEST);
 
@@ -27,30 +30,49 @@ if (array_key_exists('event_id', $_REQUEST) && ctype_digit($_REQUEST['event_id']
 // if being run on a blank database then default to add new data - maybe this should be checked later when listing all events (check if size of array is zero)...
 if (array_key_exists('action',$_REQUEST)) {
 		$action=$_REQUEST['action'];
+	} elseif (isset($event_id)) {
+		$action='view';
 	} else {
 		$total=$db->getOne('SELECT COUNT(*) FROM events e, ministry_people mp WHERE mp.pid='.$a->getPid().' AND mp.role_id<=2 AND e.ministry_id=mp.ministry_id'); //role_id of 1 and 2 indicate staff - higher is student or misc
 		if ($total==0) {
-			$_REQUEST['action']='add';
 			$action='add';
+		} else {
+			$action='list';
 		}
 }
 
-if (isset($action)) {
-	switch ($action) {
+// at this point, $action is guaranteed to be set to either add, list, view or a passed-in parameter
+
+switch ($action) {
+	case 'list':
+		$title='View All Events';
+	break;
+	case 'view':
+		$eventName=$db->getOne("SELECT name FROM events WHERE event_id=$event_id");
+		$unixDate=$db->getOne("SELECT UNIX_TIMESTAMP(begin) FROM events WHERE event_id='.$event_id");
+		$title=$eventName.' '.date('F jS, Y',$unixDate);
+		$sql='SELECT * FROM events WHERE event_id='.$event_id;
+		$result=$db->query($sql);
+		$event=$result->fetchRow();
+		$headerDetails='<em>This was '.readableTimeDiff($unixDate,time()).'</em><br/>';
+		$headerDetails.='Attendance: '.$event['estimated_attendance'].'&nbsp; ('.getEventAttendance($event_id).' signed in)<br/>';
+	break;
 	case 'add': // create a blank form for data entry
-		$event_id=NULL;
+		$title='Add New Event';
 		// this should print out a blank form for data entry
 		$result=$db->query('DESCRIBE events');
 		while ($row=$result->fetchRow()) {
 			$event[$row['Field']]='';
 		}
 		unset($event['event_id']); // we don't want the user to enter a value for this
+		$event['salvations']=0;
+		$event['baptisms_in_hs']=0;
+		$event['offering']=0;
+		$event['estimated_attendance']=0;
+		$event_id=null;
 		reset($event);
 	break;
-	case 'delete': // request confirmation for an event deletion - maybe I should do this via javascript?
-		//echo "Are you sure you want to delete this event? There is NO UNDO!<br/>";
-		//echo '<FORM action="'.$_SERVER['PHP_SELF'].'"><INPUT TYPE="SUBMIT" NAME="action" VALUE="CONFIRM"><INPUT TYPE="HIDDEN" NAME="event_id" VALUE="'.$event_id.'"></FORM>';
-//	exit();
+	case 'delete':
 		if (ownsEvent($a->getUserId(),$event_id)) {
 			$sql='DELETE FROM events WHERE event_id='.$event_id;
 			$db->exec($sql);
@@ -59,8 +81,10 @@ if (isset($action)) {
 			$message.='Event deleted.';
 			unset($event_id);
 		}
+		$title='View All Events';
 	break;
-	case 'UPDATE': // process modifications to an event
+	case 'update': // process modifications to an event
+		$title=$_POST['name'];
 		unset($_POST['action']);
 		unset($_POST['_qf__add']);
 		unset($_POST['btnSave']);
@@ -71,13 +95,14 @@ if (isset($action)) {
 		unset($_POST['end_date']);
 		unset($_POST['end_time']);
 		if (ownsEvent($a->getUserId(),$event_id)) {
-			$db->autoExecute('events',$_POST,MDB2_AUTOQUERY_UPDATE,"event_id='$event_id'");
+			$db->autoExecute('events',$_POST,MDB2_AUTOQUERY_insert,"event_id='$event_id'");
 			$message.='Event updated.';
 		} else {
 			$message.='You do not have authority to modify this event.';
 		}
 	break;
-	case 'INSERT': // this takes the results of ADD and puts it in the database
+	case 'insert': // this takes the results of ADD and puts it in the database
+		$title=$_POST['name'];
 		unset($_POST['action']);
 		unset($_POST['_qf__add']);
 		unset($_POST['btnSave']);
@@ -91,32 +116,22 @@ if (isset($action)) {
 		$_POST['event_id']=$event_id;
 		if (array_key_exists('pid',$_POST) && is_array($_POST['pid'])) {
 			foreach($_POST['pid'] as $pid) {
-				$sql="INSERT INTO event_attendance SET event_id='$event_id',pid='$pid'";
+				$sql="insert INTO event_attendance SET event_id='$event_id',pid='$pid'";
 				$db->exec($sql);
 			}
 			unset($_POST['pid']);
 		}
-		$db->autoExecute('events',$_POST,MDB2_AUTOQUERY_INSERT);
+		$db->autoExecute('events',$_POST,MDB2_AUTOQUERY_insert);
 		$message.='Event added.';
 		break;
 	default:
 		$message.="I'm sorry - I don't understand what you want me to do.";
 }
-}
-
-if (isset($event_id)) {
-	$sql='SELECT *,UNIX_TIMESTAMP(begin) as unixdate FROM events WHERE event_id='.$event_id;
-	$result=$db->query($sql);
-	$event=$result->fetchRow();
-	$name=sprintf('%s %s',$event['name'],date('F jS, Y',$event['unixdate']));
-} else {
-	$event_id=NULL;
-	$name='All Events';
-}
+ 
 ?>
 <html>
 <head>
-<title><?php echo $name;?></title>
+<title><?php echo $title;?></title>
 <link rel="stylesheet" href="css/examine.css" type="text/css">
 <link rel="stylesheet" href="css/tabs.css" type="text/css">
 <link rel="stylesheet" href="css/quickform.css" type="text/css">
@@ -219,7 +234,7 @@ YAHOO.util.Event.addListener(window, 'load', setupCal1);
 <script type='text/javascript'>new Effect.Highlight('statusmsg', {duration: 3.0});</script>
 	<div class="buttons">
 <?php
-if ((isset($action) && $action=='add') || $event_id!==null) {
+if ($action=='add' || isset($event_id) {
 ?>
 	<a href="events.php">
 	<img src="<?php echo $rooturl.'/graphics/icons/text_list_bullets.png';?>" height="16" width="16"/>
@@ -228,7 +243,7 @@ if ((isset($action) && $action=='add') || $event_id!==null) {
 	<?php
 }
 
-if (!isset($action) || $action!=='add') {
+if ($action!=='add') {
 	?>
 	<a class="positive" href="events.php?action=add">
 	<img src="<?php echo $rooturl.'/graphics/icons/add.png';?>" height="16" width="16"/>
@@ -249,13 +264,9 @@ if (isset($event_id)) {
 </div> <!-- end of buttons -->
 <br/> 
 <?php
-if (isset($event_id)) {
-		echo '<em>This was '.readableTimeDiff($event['unixdate'],time()).'</em><br/>';
-		unset($event['unixdate']);
-		echo 'Attendance: '.$event['estimated_attendance'].'&nbsp; ('.getEventAttendance($event_id).' signed in)<br/>';
-	}
+echo $headerDetails;
 
-if ($event_id===NULL && !isset($action)) {
+if ($action=='list' || $action=='delete') {
 	$sql='SELECT event_id,name,begin,UNIX_TIMESTAMP(begin) as unixdate FROM events e, ministry_people mp WHERE mp.pid='.$a->getPid().' AND mp.role_id<=2 AND e.ministry_id=mp.ministry_id ORDER BY begin DESC';
 	$result=$db->query($sql);
 	$OldTimeLabel='';
@@ -271,33 +282,31 @@ if ($event_id===NULL && !isset($action)) {
 
 	}
 	echo "</ol></ul>";
-} else { // event_id is not equal to null or we are adding an event
-	//echo '<span class="actions"><a href="#" onclick="javascript:editmode()">edit</a> | <a href='.$_SERVER['PHP_SELF'].'?action=delete&amp;event_id='.$event_id.' onclick="javascript:return confirm(\'Are you sure you want to delete this module?\')">delete</a> | <a href='.$_SERVER['PHP_SELF'].'?action=add>add a new event</a></span><br/>';
-$form = new HTML_QuickForm_DHTMLRulesTableless('add','POST',$_SERVER['PHP_SELF'],null,null,true);
-$form->addElement('header','','');
-// $form->addElement('html','<div id="cal1Container"></div>'); // used later for YUI calendar
-	   
-// add a file admin/options.events.php which will allow you to set global events options
-$eventFieldsToDisplay=$a->getPreference('eventFieldsToDisplay');
-if (!$eventFieldsToDisplay) {
-	$eventFieldsToDisplay=getSystemVariable('eventFieldsToDisplay');
-}
+} else { // detailed form view
+	$form = new HTML_QuickForm_DHTMLRulesTableless('add','POST',$_SERVER['PHP_SELF'],null,null,true);
+	$form->addElement('header','','');
+	// $form->addElement('html','<div id="cal1Container"></div>'); // used later for YUI calendar   
+	// add a file admin/options.events.php which will allow you to set global events options
+	$eventFieldsToDisplay=$a->getPreference('eventFieldsToDisplay');
+	if (!$eventFieldsToDisplay) {
+		$eventFieldsToDisplay=getSystemVariable('eventFieldsToDisplay');
+	}
 
-// maybe change this so that we check two things: if the field is set to display by default AND whether or not it is null
-if (!$eventFieldsToDisplay) { // if neither the user pref nor the system variable is set
-	$visibleFields=array_keys($event);
-} else {
-	$eventFieldsToDisplay=unserialize($eventFieldsToDisplay);
-	$visibleFields=array_intersect_key($event,$eventFieldsToDisplay);
-	$hiddenFields=array_diff_key($event,$eventFieldsToDisplay); // perhaps not necessary using this implementation
-}
+	// maybe change this so that we check two things: if the field is set to display by default AND whether or not it is null
+	if (!$eventFieldsToDisplay) { // if neither the user pref nor the system variable is set
+		$visibleFields=array_keys($event);
+	} else {
+		$eventFieldsToDisplay=unserialize($eventFieldsToDisplay);
+		$visibleFields=array_intersect_key($event,$eventFieldsToDisplay);
+		$hiddenFields=array_diff_key($event,$eventFieldsToDisplay); // perhaps not necessary using this implementation
+	}
 
-//$log->log($eventFieldsToDisplay);
-//$log->log($visibleFields);
+	//$log->log($eventFieldsToDisplay);
+	//$log->log($visibleFields);
 	   
-foreach($visibleFields as $field) {
+	foreach($visibleFields as $field) {
 	switch($field) {
-        case 'event_id':
+			case 'event_id':
             $form->addElement('hidden',$field,$field);
             // need to add these as hidden fields - although ministry_id should be a dropdown in some cases
             break;
@@ -332,19 +341,6 @@ foreach($visibleFields as $field) {
                 $event[$field.'_date']=date('n/j/Y',strtotime($event[$field]));
                 $event[$field.'_time']=date('g:ia',strtotime($event[$field]));
             }
-        //$calendar=generateYahooCalendarJS($field.'_cal',$calNum++,$field.'_date');
-                       //$form->addElement('html',$calendar);
-		/*
-			$calNum=$calNum++; // allows us to generate multiple JS calendars
-			$dateFieldName=$field.'_date';
-			$element=sprintf('<text id="%s" name="%s" type="text" value="%s"/>',$dateFieldName,$dateFieldName,$event[$field]);
-			$extra=generateYahooCalendarJS($field.'Cal',$calNum,$dateFieldName);
-	<?php
-	//todo - finish adding this js code - see if I can expand it to handle two different calendars on the same page - check Yhaoo YUI docs
-	// I still need to register it using YAHOO.example.calendar.cal1.selectEvent.subscribe(handleSelect, YAHOO.example.calendar.cal1, true); 
-	?>
-</script>
-*/
 			break;
 		case 'baptisms_in_hs':
 			$form->addElement('text',$field,'spirit baptisms');
@@ -372,54 +368,46 @@ foreach($visibleFields as $field) {
 		default:
 			$form->addElement('text',$field,$field);
 		}
-}
-$group[]=&HTML_QuickForm::createElement('xbutton', 'btnSave', '<img src="graphics/icons/tick.png" height="16" width="16"/> Save', array('class'=>'positive','onclick'=>'this.form.submit()'));
-$form->addGroup($group, null, '', ' ');
-if (isset($action) && $action=='add') {
-	$form->addElement('hidden','action','INSERT');
-	$event['salvations']=0;
-	$event['baptisms_in_hs']=0;
-	$event['offering']=0;
-	$event['estimated_attendance']=0;
-} else {
-	$form->addElement('hidden','action','UPDATE');
-}
-$form->setDefaults($event);
-$form->applyFilter('__ALL__','trim');
-$form->getValidationScript();
-      //$form->display();
-      $renderer =& new HTML_QuickForm_Renderer_Tableless();
-      $form->accept($renderer);
-	  echo '<div style="clear:both">'.$renderer->toHtml().'</div>';	
-      echo '<div id="cal1Container"></div>';
-/* @todo deal with this later
-foreach ($hiddenFields as $field) {
-	switch ($field) {
-		default:
-			$form->addElement('hidden',$field,$field);
 	}
-}
-*/
+	$group[]=&HTML_QuickForm::createElement('xbutton', 'btnSave', '<img src="graphics/icons/tick.png" height="16" width="16"/> Save', array('class'=>'positive','onclick'=>'this.form.submit()'));
+	$form->addGroup($group, null, '', ' ');
+	if ($action=='add') {
+		$form->addElement('hidden','action','insert');
+	} else {
+		$form->addElement('hidden','action','update');
+	}
+	$form->setDefaults($event);
+	$form->applyFilter('__ALL__','trim');
+	$form->getValidationScript();
+    //$form->display();
+    $renderer =& new HTML_QuickForm_Renderer_Tableless();
+    $form->accept($renderer);
+	echo '<div style="clear:both">'.$renderer->toHtml().'</div>';	
+    echo '<div id="cal1Container"></div>';
+	/* @todo deal with this later
+	foreach ($hiddenFields as $field) {
+		switch ($field) {
+			default:
+				$form->addElement('hidden',$field,$field);
+		}
+	}
+	*/
 	echo '<a href="#">view all possible fields</a></div>';
 	?>
 	</div>
 	<?php
-} // end if $event_id===NULL
+} 
 // display those present -> do this as a javascript replace to allow for dynamic updating
-if ($event_id!==NULL) {
+if (isset($event_id)) {
     ?>
 		<br/>
         <div id="eventattenders" class="subform">
         </div>
         <script type="text/javascript">
 		new Ajax.Updater('eventattenders','subforms/event.attendance.php',{parameters: 'event_id=<?php echo $event_id;?>', evalScripts: true});
-    //var req = new DataRequestor();
-    //req.setObjToReplace('eventattenders');
-    //req.addArg(_GET, "event_id", "<?php echo $event_id;?>");
-    //req.getURL('subforms/event.attendance.php');
-    </script>
+		</script>
         <?php
-} // end $event_id !==NULL
+}
 ?>
 </div>
 <?php include 'templates/footer.php';?>
